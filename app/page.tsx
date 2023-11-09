1d92355dd5f09fcfea76c00e01a736601374d67e
Jesse <purplecabbage@gmail.com>
2023-11-08 20:18:42 -0800

    first post

--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,21 @@
 import { allPosts } from "@/.contentlayer/generated"
 import Link from "next/link"
-import ConvertkitSignupForm from "@/components/ConvertkitSignupForm"
+// import ConvertkitSignupForm from "@/components/ConvertkitSignupForm"
 
 export default function Home() {
   return (
     <div className="prose dark:prose-invert min-w-full">
-      <article key='comingSoon' className='m-auto'>
-        <h4>Coming Soon ...</h4>
-        <img src='/RisingJ-1.png' alt='Rising J Upcoming' className='comingSoon'/>
-        <p>A new look, a new focus, a new day.</p>
-      </article>
-      {/* {allPosts.map((post) => (
+      { allPosts.slice(0,1).map((post) => (
         <article key={post._id}>
           <Link href={post.slug}>
             <h2>{post.title}</h2>
           </Link>
           {post.description && <p>{post.description}</p>}
         </article>
-      ))} */}
-      {/* <ConvertkitSignupForm formId={52}/> */}
+      ))}
+      <article key='comingSoon' className='m-auto'>
+        <img src='/RisingJ-1.png' alt='Rising J Upcoming' className='comingSoon'/>
+      </article>
     </div>
   )
 }
