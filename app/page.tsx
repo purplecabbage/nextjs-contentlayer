7ef75ae317744c78ae43ae8b4f6395f8b012f4a7
Jesse <purplecabbage@gmail.com>
2024-05-19 01:18:59 -0700

    Add latest release

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,13 @@
 import { allPosts } from "@/.contentlayer/generated"
+import StreamLinks from "@/components/StreamLinks"
 import Link from "next/link"
-import ConvertkitSignupForm from "@/components/ConvertkitSignupForm"
+
 
 export default function Home() {
   return (
-    <div className="prose dark:prose-invert min-w-full">
-      { allPosts.slice(0,1).map((post) => (
-        <article key={post._id} className="px-5 sm:px-20">
-          <Link href={post.slug}>
-            <h2>{post.title}</h2>
-          </Link>
-          {post.description && <p>{post.description}</p>}
-        </article>
-      ))}
+    <div className="min-w-full">
       <article key='comingSoon' className='m-auto'>
-        <img src='/RisingJ-1.png' alt='Rising J Upcoming' className='comingSoon'/>
+        <StreamLinks/>
       </article>
     </div>
   )
